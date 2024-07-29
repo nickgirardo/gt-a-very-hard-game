8253312714a4260038934e69b285d4412f432ac7
Nick Girardo <nickgirardo@gmail.com>
2024-07-29 13:50:01 -0400

    Prefer void * in save_write

--- src/gt/persist.c
+++ src/gt/persist.c
@@ -59,7 +59,7 @@ void lock_bypass() {
 
 //src assumed not Flash ROM
 //dest assumned to be in Flash ROM
-void save_write(char* src, char* dest, char len) {
+void save_write(void *src, void *dest, char len) {
     *dma_flags = flagsMirror & ~(DMA_IRQ | DMA_NMI);
     asm("SEI");
     change_rom_bank(SAVE_BANK_NUM);
@@ -68,8 +68,8 @@ void save_write(char* src, char* dest, char len) {
     unlock_bypass();
     while(k) {
         *((unsigned char*) 0x8000) = 0xA0;
-        dest[i] = src[i];
-        while(dest[i] != src[i]) {
+	((char *)dest)[i] = ((char *)src)[i];
+        while(((char *)dest)[i] != ((char *)src)[i]) {
             
         }
         i++;
